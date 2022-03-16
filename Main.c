9243f81732205f4473d4be2706ba0576d8169f9f
dvirGev <dvir87879@gmail.com>
2022-03-16 14:51:04 +0200

    daniel will do it

--- Main.c
+++ Main.c
@@ -1,21 +1,23 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<dirent.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
 
 #define lenght 50
 
-void printDir(){
-    DIR * dir;
+void printDir()
+{
+    DIR *dir;
     struct dirent *sdir;
-     dir=opendir(".");
-     if(dir==NULL){
-         printf("Erro Somthing Bad Happened :(\n");
-         return;
-     }
-    while((sdir = readdir(dir)) != NULL){
+    dir = opendir(".");
+    if (dir == NULL)
+    {
+        printf("Erro Somthing Bad Happened :(\n");
+        return;
+    }
+    while ((sdir = readdir(dir)) != NULL)
+    {
         if (sdir->d_name[0] != '.')
         {
             printf("%s\t", sdir->d_name);
@@ -23,12 +25,10 @@ void printDir(){
     }
     printf("\n");
     closedir(dir);
-
-
 }
-void updateLocation(char* location)
+void updateLocation(char *location)
 {
-    if(getcwd(location, lenght) == NULL)
+    if (getcwd(location, lenght) == NULL)
     {
         printf("Erro Somthing Bad Happened :(\n");
         return 1;
@@ -38,25 +38,27 @@ int main(int argc, char const *argv[])
 {
     char command[lenght];
     char location[lenght];
-    
+
     while (strcmp(command, "EXIT"))
     {
         updateLocation(location);
-        // printf("yes master>$ "); 
+        // printf("yes master>$ ");
         printf("%s>", location);
         gets(command);
         if (strncmp(command, "ECHO ", 5) == 0)
         {
             for (int i = 5; i < lenght && command[i] != '\0'; i++)
-            {    
+            {
                 printf("%c", command[i]);
             }
             printf("\n");
         }
-        else if(strcmp(command,"DIR")==0){
+        else if (strcmp(command, "DIR") == 0)
+        {
             printDir();
         }
-        else if(strncmp(command,"CD ", 3)==0){
+        else if (strncmp(command, "CD ", 3) == 0)
+        {
             if (chdir(&(command[3])) != 0)
             {
                 printf("Erro Somthing Bad Happened :(\n");
@@ -64,10 +66,15 @@ int main(int argc, char const *argv[])
             }
             // chdir היא פונקצית קריאה למערכת
         }
+        else if (strncmp(command, "COPY ", 5) == 0)
+        {
+        
+        }
         else
         {
-            system(command);
+            //system(command);
             // system היא פונקצית קריאה למערכת
+            int chek = fork();
         }
     }
     return 0;
