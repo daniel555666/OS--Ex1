7f30b0ca1cf4cf78a67223632dd939ccacf9578d
daniel555666 <danielzaken5@gmail.com>
2022-03-16 13:36:32 +0200

    2

--- Main.c
+++ Main.c
@@ -1,31 +1,54 @@
 #include<stdio.h>
 #include<string.h>
 #include<unistd.h>
+#include<dirent.h>
+
 
 #define lenght 50
 
+void printDir(){
+    DIR * dir;
+    struct dirnet *sdir;
+     dir=opendir(".");
+     if(dir==NULL){
+         printf("Erro Somthing Bad Happened :(\n");
+         return;
+     }
+    while((sdir=readdir(sdir))!=NULL){
+        printf("%s/t", sdir->d_name);
+
+    }
+    printf("/n");
+    closedir(dir);
+
+
+}
+
 int main(int argc, char const *argv[])
 {
-    char comand[lenght];
+    char command[lenght];
     char location[lenght];
     if(getcwd(location, lenght) == NULL)
     {
         printf("Erro Somthing Bad Happened :(\n");
         return 1;
     }
-    while (strcmp(comand, "EXIT"))
+    while (strcmp(command, "EXIT"))
     {
         // printf("yes master>$ "); 
         printf("%s>", location);
-        gets(comand);
+        gets(command);
 
-        if (strncmp(comand, "ECHO ", 5) == 0)
+        if (strncmp(command, "ECHO ", 5) == 0)
         {
-            for (int i = 5; i < lenght && comand[i] != '\0'; i++)
+            for (int i = 5; i < lenght && command[i] != '\0'; i++)
             {    
-                printf("%c", comand[i]);
+                printf("%c", command[i]);
             }
             printf("\n");
+        }
+        else if(strcmp(command,"DIR")==0){
+            printDir();
         }
     }
     return 0;
